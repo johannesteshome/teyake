185185da1694788e4730128a01643c6e5175bb3d
yohannesME <yohannesmesg@gmail.com>
2022-06-08 09:18:41 -0700

    update database

--- database/teyake.sql
+++ database/teyake.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 06, 2022 at 08:23 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.28
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 08, 2022 at 04:17 PM
+-- Server version: 8.0.11
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,11 +27,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `answer`
 --
 
-CREATE TABLE `answer` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE IF NOT EXISTS `answer` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `ExamKey` varchar(11) NOT NULL,
-  `AnswerList` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `AnswerList` longtext NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ans_exam_key` (`ExamKey`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `answer`
@@ -49,11 +52,14 @@ INSERT INTO `answer` (`ID`, `ExamKey`, `AnswerList`) VALUES
 -- Table structure for table `course`
 --
 
-CREATE TABLE `course` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE IF NOT EXISTS `course` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(35) NOT NULL,
-  `DeptID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `DeptID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `course_dept_id` (`DeptID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `course`
@@ -75,10 +81,12 @@ INSERT INTO `course` (`ID`, `Name`, `DeptID`) VALUES
 -- Table structure for table `department`
 --
 
-CREATE TABLE `department` (
-  `ID` int(11) NOT NULL,
-  `Name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE IF NOT EXISTS `department` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(30) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `department`
@@ -99,7 +107,8 @@ INSERT INTO `department` (`ID`, `Name`) VALUES
 -- Table structure for table `exam`
 --
 
-CREATE TABLE `exam` (
+DROP TABLE IF EXISTS `exam`;
+CREATE TABLE IF NOT EXISTS `exam` (
   `Name` varchar(60) NOT NULL,
   `CourseID` int(11) DEFAULT NULL,
   `ExamKey` varchar(5) NOT NULL,
@@ -108,8 +117,13 @@ CREATE TABLE `exam` (
   `ExaminerID` int(11) NOT NULL,
   `Status` varchar(10) DEFAULT NULL,
   `Duration` int(10) NOT NULL,
-  `Date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Date` date NOT NULL,
+  PRIMARY KEY (`ExamKey`),
+  KEY `exam_ans_id` (`AnswerID`),
+  KEY `exam_course_id` (`CourseID`),
+  KEY `exam_examiner_id` (`ExaminerID`),
+  KEY `exam_question_id` (`QuestionID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `exam`
@@ -127,7 +141,8 @@ INSERT INTO `exam` (`Name`, `CourseID`, `ExamKey`, `QuestionID`, `AnswerID`, `Ex
 -- Table structure for table `examinee`
 --
 
-CREATE TABLE `examinee` (
+DROP TABLE IF EXISTS `examinee`;
+CREATE TABLE IF NOT EXISTS `examinee` (
   `ID` int(11) NOT NULL,
   `FullName` varchar(60) NOT NULL,
   `Email` varchar(60) NOT NULL,
@@ -136,18 +151,10 @@ CREATE TABLE `examinee` (
   `InstID` int(11) DEFAULT NULL,
   `ExamKey` varchar(25) NOT NULL,
   `Sex` char(1) NOT NULL,
-  `Score` float DEFAULT 0,
-  `AnswerList` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '[]',
+  `Score` float DEFAULT '0',
+  `AnswerList` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   `SchoolID` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `examinee`
---
-
-INSERT INTO `examinee` (`ID`, `FullName`, `Email`, `Section`, `DeptID`, `InstID`, `ExamKey`, `Sex`, `Score`, `AnswerList`, `SchoolID`) VALUES
-(174, 'Yohannes Assefa', 'yohannes@mail.com', 'D', NULL, 1, 'BIVke', 'M', 1, '[null,2,2]', '0704/12'),
-(175, 'Yohannes Assefa', 'yohannes@mail.com', 'D', NULL, 1, 'beEit', 'M', 1, '[3,2]', '0704/12');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -155,8 +162,9 @@ INSERT INTO `examinee` (`ID`, `FullName`, `Email`, `Section`, `DeptID`, `InstID`
 -- Table structure for table `examiner`
 --
 
-CREATE TABLE `examiner` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `examiner`;
+CREATE TABLE IF NOT EXISTS `examiner` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `FullName` varchar(60) NOT NULL,
   `Email` varchar(60) NOT NULL,
   `Password` varchar(60) NOT NULL,
@@ -165,8 +173,11 @@ CREATE TABLE `examiner` (
   `InstID` int(11) DEFAULT NULL,
   `PhoneNo` bigint(20) NOT NULL,
   `ImageURL` varchar(60) DEFAULT NULL,
-  `verified` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `verified` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `examiner_dept_id` (`DeptID`),
+  KEY `examiner_inst_id` (`InstID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `examiner`
@@ -183,13 +194,15 @@ INSERT INTO `examiner` (`ID`, `FullName`, `Email`, `Password`, `Sex`, `DeptID`, 
 -- Table structure for table `inprogressexams`
 --
 
-CREATE TABLE `inprogressexams` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `inprogressexams`;
+CREATE TABLE IF NOT EXISTS `inprogressexams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `Email` varchar(60) NOT NULL,
   `ExamKey` varchar(5) NOT NULL,
   `AnswerList` longtext NOT NULL,
-  `EndTime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `EndTime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -197,12 +210,14 @@ CREATE TABLE `inprogressexams` (
 -- Table structure for table `institution`
 --
 
-CREATE TABLE `institution` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `institution`;
+CREATE TABLE IF NOT EXISTS `institution` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(60) NOT NULL,
   `Address` varchar(60) NOT NULL,
-  `Phone` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Phone` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `institution`
@@ -219,11 +234,14 @@ INSERT INTO `institution` (`ID`, `Name`, `Address`, `Phone`) VALUES
 -- Table structure for table `question`
 --
 
-CREATE TABLE `question` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE IF NOT EXISTS `question` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `ExamKey` varchar(11) NOT NULL,
-  `QuestionList` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `QuestionList` longtext NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `question_exam_key` (`ExamKey`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `question`
@@ -235,126 +253,19 @@ INSERT INTO `question` (`ID`, `ExamKey`, `QuestionList`) VALUES
 (15, 'WQusP', '[[\"Test the what?\",\"Big Yes\",\"Nah fam\",null,null,null],[\"This is a second one?\",\"Maybe\",\"Probably\",\"Yes\",null,null],[\"Applebeans\",\"Based\",\"Cap\",null,null,null]]'),
 (16, 'duuAM', '[[\"What is the?\",\"Naaa Bruv\",\"Yee\",null,null,null],[\"This is a second one?\",\"Maybe\",\"Probably\",\"Yes\",\"Nope\",null]]');
 
---
--- Indexes for dumped tables
---
+-- --------------------------------------------------------
 
 --
--- Indexes for table `answer`
---
-ALTER TABLE `answer`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `ans_exam_key` (`ExamKey`);
-
---
--- Indexes for table `course`
---
-ALTER TABLE `course`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `course_dept_id` (`DeptID`);
-
---
--- Indexes for table `department`
---
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `exam`
---
-ALTER TABLE `exam`
-  ADD PRIMARY KEY (`ExamKey`),
-  ADD KEY `exam_ans_id` (`AnswerID`),
-  ADD KEY `exam_course_id` (`CourseID`),
-  ADD KEY `exam_examiner_id` (`ExaminerID`),
-  ADD KEY `exam_question_id` (`QuestionID`);
-
---
--- Indexes for table `examinee`
---
-ALTER TABLE `examinee`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `examinee_dept_id` (`DeptID`),
-  ADD KEY `examinee_exam_key` (`ExamKey`);
-
---
--- Indexes for table `examiner`
---
-ALTER TABLE `examiner`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `examiner_dept_id` (`DeptID`),
-  ADD KEY `examiner_inst_id` (`InstID`);
-
---
--- Indexes for table `inprogressexams`
---
-ALTER TABLE `inprogressexams`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `institution`
---
-ALTER TABLE `institution`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `question`
---
-ALTER TABLE `question`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `question_exam_key` (`ExamKey`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Table structure for table `requests`
 --
 
---
--- AUTO_INCREMENT for table `answer`
---
-ALTER TABLE `answer`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `course`
---
-ALTER TABLE `course`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `department`
---
-ALTER TABLE `department`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `examinee`
---
-ALTER TABLE `examinee`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=176;
-
---
--- AUTO_INCREMENT for table `examiner`
---
-ALTER TABLE `examiner`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `inprogressexams`
---
-ALTER TABLE `inprogressexams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
-
---
--- AUTO_INCREMENT for table `institution`
---
-ALTER TABLE `institution`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `question`
---
-ALTER TABLE `question`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+DROP TABLE IF EXISTS `requests`;
+CREATE TABLE IF NOT EXISTS `requests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `input` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Constraints for dumped tables
@@ -364,42 +275,35 @@ ALTER TABLE `question`
 -- Constraints for table `answer`
 --
 ALTER TABLE `answer`
-  ADD CONSTRAINT `ans_exam_key` FOREIGN KEY (`ExamKey`) REFERENCES `exam` (`ExamKey`);
+  ADD CONSTRAINT `ans_exam_key` FOREIGN KEY (`ExamKey`) REFERENCES `exam` (`examkey`);
 
 --
 -- Constraints for table `course`
 --
 ALTER TABLE `course`
-  ADD CONSTRAINT `course_dept_id` FOREIGN KEY (`DeptID`) REFERENCES `department` (`ID`);
+  ADD CONSTRAINT `course_dept_id` FOREIGN KEY (`DeptID`) REFERENCES `department` (`id`);
 
 --
 -- Constraints for table `exam`
 --
 ALTER TABLE `exam`
-  ADD CONSTRAINT `exam_ans_id` FOREIGN KEY (`AnswerID`) REFERENCES `answer` (`ID`),
-  ADD CONSTRAINT `exam_course_id` FOREIGN KEY (`CourseID`) REFERENCES `course` (`ID`),
-  ADD CONSTRAINT `exam_examiner_id` FOREIGN KEY (`ExaminerID`) REFERENCES `examiner` (`ID`),
-  ADD CONSTRAINT `exam_question_id` FOREIGN KEY (`QuestionID`) REFERENCES `question` (`ID`);
-
---
--- Constraints for table `examinee`
---
-ALTER TABLE `examinee`
-  ADD CONSTRAINT `examinee_dept_id` FOREIGN KEY (`DeptID`) REFERENCES `department` (`ID`),
-  ADD CONSTRAINT `examinee_exam_key` FOREIGN KEY (`ExamKey`) REFERENCES `exam` (`ExamKey`);
+  ADD CONSTRAINT `exam_ans_id` FOREIGN KEY (`AnswerID`) REFERENCES `answer` (`id`),
+  ADD CONSTRAINT `exam_course_id` FOREIGN KEY (`CourseID`) REFERENCES `course` (`id`),
+  ADD CONSTRAINT `exam_examiner_id` FOREIGN KEY (`ExaminerID`) REFERENCES `examiner` (`id`),
+  ADD CONSTRAINT `exam_question_id` FOREIGN KEY (`QuestionID`) REFERENCES `question` (`id`);
 
 --
 -- Constraints for table `examiner`
 --
 ALTER TABLE `examiner`
-  ADD CONSTRAINT `examiner_dept_id` FOREIGN KEY (`DeptID`) REFERENCES `department` (`ID`),
-  ADD CONSTRAINT `examiner_inst_id` FOREIGN KEY (`InstID`) REFERENCES `institution` (`ID`);
+  ADD CONSTRAINT `examiner_dept_id` FOREIGN KEY (`DeptID`) REFERENCES `department` (`id`),
+  ADD CONSTRAINT `examiner_inst_id` FOREIGN KEY (`InstID`) REFERENCES `institution` (`id`);
 
 --
 -- Constraints for table `question`
 --
 ALTER TABLE `question`
-  ADD CONSTRAINT `question_exam_key` FOREIGN KEY (`ExamKey`) REFERENCES `exam` (`ExamKey`);
+  ADD CONSTRAINT `question_exam_key` FOREIGN KEY (`ExamKey`) REFERENCES `exam` (`examkey`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
